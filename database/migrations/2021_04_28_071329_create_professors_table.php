166494083192a8503777583ce8b28e2c490e4baa
marianpascari <68700009+marianpascari@users.noreply.github.com>
2021-04-28 10:43:03 +0300

    migrations

--- database/migrations/2021_04_28_071329_create_professors_table.php
+++ database/migrations/2021_04_28_071329_create_professors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProfessorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('professors', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('faculty');
+            $table->string('cnp');
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('professors');
     }
 }
